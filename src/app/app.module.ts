2bc2639aaa4e7ffdb70c18dc625c5f1ba0d6f443
hasinichathu <164124V@uom.lk>
2019-05-14 18:50:25 +0530

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { NewsFeedItemComponent } from './news-feed-item/news-feed-item.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+import { environment } from 'src/environments/environment.prod';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FeedControllerService } from './Services/feed-controller.service';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsFeedComponent,
+    NewsFeedItemComponent,
+    FirstPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FeedControllerService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBxeWqKBS_rmuM1Sl8UW9hvDgwKTQ9HVvM",
+    authDomain: "social-network-c3da4.firebaseapp.com",
+    databaseURL: "https://social-network-c3da4.firebaseio.com",
+    projectId: "social-network-c3da4",
+    storageBucket: "social-network-c3da4.appspot.com",
+    messagingSenderId: "728504784714"
+  }
 };
