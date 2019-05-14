98ece1dc60bc5c4a949c669d4b0af2ab7c10da61
hasinichathu <164124V@uom.lk>
2019-05-14 18:21:50 +0530

    dfd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBxeWqKBS_rmuM1Sl8UW9hvDgwKTQ9HVvM",
+    authDomain: "social-network-c3da4.firebaseapp.com",
+    databaseURL: "https://social-network-c3da4.firebaseio.com",
+    projectId: "social-network-c3da4",
+    storageBucket: "social-network-c3da4.appspot.com",
+    messagingSenderId: "728504784714"
+  }
 };
 
 /*
